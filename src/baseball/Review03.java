0fc60207d46534f93613580a092d6c9fa8a78f1a
MiRuJt <ai_miru_ikai@yahoo.co.jp>
2023-02-26 10:02:51 +0900

    BaseBallTeamとReaview03新規作成

---
+++ src/baseball/Review03.java
@@ -0,0 +1,30 @@
+package baseball;
+
+public class Review03 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+        BaseBallTeam test01 = new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);
+
+        BaseBallTeam test02 = new BaseBallTeam("横浜DeNAベイスターズ",73,68,2);
+
+        BaseBallTeam test03 = new BaseBallTeam("阪神タイガース",68,71,4);
+
+        BaseBallTeam test04 = new BaseBallTeam("読売ジャイアンツ",68,72,3);
+
+        BaseBallTeam test05 = new BaseBallTeam("広島東洋カープ",66,74,3);
+
+        BaseBallTeam test06 = new BaseBallTeam("中日ドラゴンズ",66,75,2);
+
+
+        test01.report();
+        test02.report();
+        test03.report();
+        test04.report();
+        test05.report();
+        test06.report();
+
+    }
+
+}
